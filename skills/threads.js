0879f653919d616744e156dc5287379ec588132c
weigang <weigang@cisco.com>
2018-03-22 09:40:50 -0700

    test

--- skills/threads.js
+++ skills/threads.js
@@ -6,7 +6,7 @@ module.exports = function (controller) {
 
             convo.ask("What is your favorite color from threads?", [
                 {
-                    pattern: "^blue|green|pink|red|yellow$",
+                    pattern: "^blue|green|pink|red|yellow|CSC*$",
                     callback: function (response, convo) {
                         convo.gotoThread("success");
                     },
