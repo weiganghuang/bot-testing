c368846a81bf832dd0ed7b5682dcac70e766bd76
weigang <weigang@cisco.com>
2018-03-22 13:44:43 -0700

    test

--- skills/cdets.js
+++ skills/cdets.js
@@ -9,6 +9,24 @@ module.exports = function (controller) {
                     pattern: "^CSC",
                     callback: function (response, convo) {
                         convo.say('You entered  ' + response.text + ' I will get the information');
+                        var ddts = response.text;
+                        var secret = "ZIaGE2sqvCqND2T0KcqCwef0sydQs1GC";
+                        var key = "8f21b566-3ba1-4aa8-a629-e109f6c1bbb2";
+                        var url = "https://cdetsng.cisco.com/wsapi/LTS-5.0/api/bug/${ddts}/note/Release-note";
+                        var request = require("request");
+
+var options = { method: 'GET',
+  url: 'https://cdetsng.cisco.com/wsapi/LTS-5.0/api/bug/CSCun66310/note/Release-note',
+  headers: 
+   { 'Postman-Token': 'ea7b5e5f-164f-4de2-ba3e-66d2a21ca011',
+     'Cache-Control': 'no-cache',
+     Authorization: 'OAuth oauth_consumer_key=\\"8f21b566-3ba1-4aa8-a629-e109f6c1bbb2\\",oauth_signature_method=\\"HMAC-SHA1\\",oauth_timestamp=\\"1521751302\\",oauth_nonce=\\"ZI9Vas1cdcS\\",oauth_version=\\"1.0\\",oauth_signature=\\"Av8E1%2B3RBB7E2j6wVtOGtWUFWEA%3D\\"' } };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
                         convo.next();
                     },
                 },
