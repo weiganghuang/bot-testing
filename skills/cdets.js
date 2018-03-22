c5734507626574f36c92fde4fdd9314a90999a4d
weigang <weigang@cisco.com>
2018-03-22 10:29:46 -0700

    test

---
+++ skills/cdets.js
@@ -0,0 +1,26 @@
+module.exports = function (controller) {
+
+    controller.hears([/^CDETS$/], "direct_message,direct_mention", function (bot, message) {
+
+        bot.startConversation(message, function (err, convo) {
+
+            convo.ask("CDETS?", [
+                {
+                    pattern: "^blue|green|pink|red|yellow|CSC",
+                    callback: function (response, convo) {
+                        convo.say('Cool, I like ' + response.text + ' too!');
+                        convo.next();
+                    },
+                },
+                {
+                    default: true,
+                    callback: function (response, convo) {
+                        convo.say("Sorry, I don't know this color. Try another one...");
+                        convo.repeat();
+                        convo.next();
+                    }
+                }
+            ]);
+        });
+    });
+};
