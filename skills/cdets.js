0ef758b7db0d65ca5a97f80d10012f1649cbc362
weigang <weigang@cisco.com>
2018-03-22 14:20:45 -0700

    test

--- skills/cdets.js
+++ skills/cdets.js
@@ -14,19 +14,20 @@ module.exports = function (controller) {
                         var key = "8f21b566-3ba1-4aa8-a629-e109f6c1bbb2";
                         var url = "https://cdetsng.cisco.com/wsapi/LTS-5.0/api/bug/${ddts}/note/Release-note";
                         var request = require("request");
-
-var options = { method: 'GET',
-  url: 'https://cdetsng.cisco.com/wsapi/LTS-5.0/api/bug/CSCun66310/note/Release-note',
-  headers: 
-   { 'Postman-Token': 'ea7b5e5f-164f-4de2-ba3e-66d2a21ca011',
-     'Cache-Control': 'no-cache',
-     Authorization: 'OAuth oauth_consumer_key=\\"8f21b566-3ba1-4aa8-a629-e109f6c1bbb2\\",oauth_signature_method=\\"HMAC-SHA1\\",oauth_timestamp=\\"1521751302\\",oauth_nonce=\\"ZI9Vas1cdcS\\",oauth_version=\\"1.0\\",oauth_signature=\\"Av8E1%2B3RBB7E2j6wVtOGtWUFWEA%3D\\"' } };
-bot.reoly(message,'about to send request');
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
-  conv.say('yes  ' + body)
-  console.log(body);
-});
+                        bot.reoly(message,'about to send request');
+                        var options = { method: 'GET',
+                        url: 'https://cdetsng.cisco.com/wsapi/LTS-5.0/api/bug/CSCun66310/note/Release-note',
+                        headers: 
+                         { 'Postman-Token': 'ea7b5e5f-164f-4de2-ba3e-66d2a21ca011',
+                           'Cache-Control': 'no-cache',
+                           Authorization: 'OAuth oauth_consumer_key=\\"8f21b566-3ba1-4aa8-a629-e109f6c1bbb2\\",oauth_signature_method=\\"HMAC-SHA1\\",oauth_timestamp=\\"1521751302\\",oauth_nonce=\\"ZI9Vas1cdcS\\",oauth_version=\\"1.0\\",oauth_signature=\\"Av8E1%2B3RBB7E2j6wVtOGtWUFWEA%3D\\"' } };
+                           
+                           request(options, function (error, response, body) {
+                            if (error) throw new Error(error);
+                            conv.say('yes  ' + body)
+                            console.log(body);
+                        });
+                        
                         convo.next();
                     },
                 },
