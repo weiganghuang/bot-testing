4e4550313272684b428a9f747c557245fe48664f
weigang <weigang@cisco.com>
2018-03-22 16:10:58 -0700

    test

--- skills/color.js
+++ skills/color.js
@@ -16,45 +16,22 @@ module.exports = function (controller) {
 };
 
 function query(convo) {
-    var http = require("https");
+    var unirest = require("unirest");
 
-    var options = {
-        "method": "GET",
-        "hostname": [
-            "cdetsng",
-            "cisco",
-            "com"
-        ],
-        "path": [
-            "wsapi",
-            "LTS-5.0",
-            "api",
-            "bug",
-            "CSCun66310",
-            "note",
-            "Release-note"
-        ],
-        "headers": {
-            "Authorization": "OAuth oauth_consumer_key=\\\"8f21b566-3ba1-4aa8-a629-e109f6c1bbb2\\\",oauth_signature_method=\\\"HMAC-SHA1\\\",oauth_timestamp=\\\"1521751302\\\",oauth_nonce=\\\"ZI9Vas1cdcS\\\",oauth_version=\\\"1.0\\\",oauth_signature=\\\"Av8E1%2B3RBB7E2j6wVtOGtWUFWEA%3D\\\"",
-            "Cache-Control": "no-cache",
-            "Postman-Token": "d2a3175d-b59a-4dd7-ac0d-67666e8c43d0"
-        }
-    };
-
-    var req = http.request(options, function (res) {
-        var chunks = [];
-
-        res.on("data", function (chunk) {
-            chunks.push(chunk);
-        });
-
-        res.on("end", function () {
-            var body = Buffer.concat(chunks);
-            //convo.say('done query')
-            //console.log(body.toString());
-        });
+    var req = unirest("GET", "https://cdetsng.cisco.com/wsapi/LTS-5.0/api/bug/CSCun66310/note/Release-note");
+    
+    req.headers({
+      "Postman-Token": "e45590e2-79b2-4f8c-a554-2a4d513ed072",
+      "Cache-Control": "no-cache",
+      "Authorization": "OAuth oauth_consumer_key=\\\"8f21b566-3ba1-4aa8-a629-e109f6c1bbb2\\\",oauth_signature_method=\\\"HMAC-SHA1\\\",oauth_timestamp=\\\"1521759362\\\",oauth_nonce=\\\"KThl8SvRIuw\\\",oauth_version=\\\"1.0\\\",oauth_signature=\\\"oMoXqBrVVimd6kzz%2B8sAld9g48I%3D\\\""
     });
-
-    req.end(); 
+    
+    
+    req.end(function (res) {
+      if (res.error) throw new Error(res.error);
+    
+      console.log(res.body);
+    });
+    
 }
 
