57992470be505e7e1ef8573b85d8f58676a59517
weigang <weigang@cisco.com>
2018-03-22 18:18:02 -0700

    test

--- skills/color.js
+++ skills/color.js
@@ -56,7 +56,7 @@ var req = http.request(options, function (res) {
 
   res.on("end", function () {
     var body = Buffer.concat(chunks);
-    console.log(body.toString());
+    //console.log(body.toString());
   });
 });
 
