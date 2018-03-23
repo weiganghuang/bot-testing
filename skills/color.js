6056445d0da3954cb1a5a6d5997ebc6ee2416ee5
weigang <weigang@cisco.com>
2018-03-22 18:21:21 -0700

    test

--- skills/color.js
+++ skills/color.js
@@ -22,44 +22,18 @@ function invoke() {
 
 }
 function query(convo) {
-    var http = require("https");
+    var request = require("request");
 
-var options = {
-  "method": "GET",
-  "hostname": [
-    "cdetsng",
-    "cisco",
-    "com"
-  ],
-  "path": [
-    "wsapi",
-    "LTS-5.0",
-    "api",
-    "bug",
-    "CSCun66310",
-    "note",
-    "Release-note"
-  ],
-  "headers": {
-    "Authorization": "OAuth oauth_consumer_key=\\\"8f21b566-3ba1-4aa8-a629-e109f6c1bbb2\\\",oauth_signature_method=\\\"HMAC-SHA1\\\",oauth_timestamp=\\\"1521760724\\\",oauth_nonce=\\\"QQniicDwLyO\\\",oauth_version=\\\"1.0\\\",oauth_signature=\\\"NEoMFSzF6Ow7%2FUufWt5j294ybh0%3D\\\"",
-    "Cache-Control": "no-cache",
-    "Postman-Token": "493819fc-5846-4f43-85a0-be973a78219c"
-  }
-};
-
-var req = http.request(options, function (res) {
-  var chunks = [];
-
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
-
-  res.on("end", function () {
-    var body = Buffer.concat(chunks);
-    //console.log(body.toString());
-  });
-});
-
-req.end();
-}
-
+    var options = { method: 'GET',
+      url: 'https://cdetsng.cisco.com/wsapi/LTS-5.0/api/bug/CSCun66310/note/Release-note',
+      headers: 
+       { 'Postman-Token': '36b533c7-a276-4633-9e4d-f73d27bacd32',
+         'Cache-Control': 'no-cache',
+         Authorization: 'OAuth oauth_consumer_key=\\"8f21b566-3ba1-4aa8-a629-e109f6c1bbb2\\",oauth_signature_method=\\"HMAC-SHA1\\",oauth_timestamp=\\"1521760724\\",oauth_nonce=\\"QQniicDwLyO\\",oauth_version=\\"1.0\\",oauth_signature=\\"NEoMFSzF6Ow7%2FUufWt5j294ybh0%3D\\"' } };
+    
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+    
+      console.log(body);
+    });
+}    
