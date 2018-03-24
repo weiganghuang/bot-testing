7f90a909a18b335e6d8174dba6e819c79294bf74
weigang <weigang@cisco.com>
2018-03-24 00:35:57 -0700

    test

--- skills/cdets.js
+++ skills/cdets.js
@@ -3,21 +3,62 @@
 //
 module.exports = function (controller) {
 
-    controller.hears([/^CDETS$/], 'direct_message,direct_mention', function (bot, message) {
+    controller.hears([/^cdets$/], 'direct_message,direct_mention', function (bot, message) {
 
-        // Check if a User preference already exists
-        var userId = message.raw_message.actorId;
-        controller.storage.users.get(userId, function (err, data) {
-            if (err) {
-                bot.reply(message, 'could not access storage, err: ' + err.message, function (err, message) {
-                    bot.reply(message, 'sorry, I am not feeling well \uF613! try again later...');
-                });
-                return;
+        bot.startConversation(message, function (err, convo) {
+            convo.say('This is a CDETS bot sample.');
+            convo.ask('What is your CDETS id?', [
+               { 
+                pattern: "^CSC",
+                callback: function (response, convo) {                 
+                    convo.say("Ok, I got '" + response.text );
+                    convo.say("Let me get the details for you");
+                    var cdets = response.text;
+                    console.log('cdets:'+ cdets + 'xxx');
+                    var request = require("request");
+                    var urlvar = 'https://cdetsng.cisco.com/wsapi/LTS-5.0/api/bug/' + cdets.toString() +'/note/Release-note';
+                    console.log('url: '+ urlvar);
+                    var options = { 
+                        method: 'GET',
+                        url: urlvar,
+                        timeout: 300000,
+                        oauth:{
+                            consumer_key:'8f21b566-3ba1-4aa8-a629-e109f6c1bbb2',
+                            consumer_secret:'ZIaGE2sqvCqND2T0KcqCwef0sydQs1GC',
+                        },
+                        /* headers: {
+                                     
+                            Authorization: 'OAuth oauth_consumer_key=\"8f21b566-3ba1-4aa8-a629-e109f6c1bbb2\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"1521760724\",oauth_nonce=\"QQniicDwLyO\",oauth_version=\"1.0\",oauth_signature=\"NEoMFSzF6Ow7%2FUufWt5j294ybh0%3D\"' 
+                        }  */
+                    };
+
+                    var str = '';
+                    request(options, function (error, response, body) {
+                        if (error) throw new Error(error);
+                        str = body.toString();
+                        convo.say('got query result');
+                        convo.say(str);
+                    
+                        console.log(body.toString());
+                        convo.next();
+                    });
+
+                },
+            },
+            {
+                default: true,
+                callback: function (response, convo) {
+                    convo.say("Sorry, CDETS starts with CSC, try again.");
+                    convo.repeat();
+                    convo.next();
+                }
             }
-            askForFavoriteColor(controller, bot, message, userId);
+        ], { key: "answer"});
         });
+
     });
-}
+};
+
 
 function askForFavoriteColor(controller, bot, message, userId) {
     bot.startConversation(message, function (err, convo) {
@@ -38,7 +79,7 @@ function askForFavoriteColor(controller, bot, message, userId) {
                         //convo.say(message,'send query');
                         //query(convo)
                         //convo.transitionTo("success", `_stored user preference_`);
-                        query(convo)
+                       
                     });
 
                 },
@@ -61,45 +102,3 @@ function askForFavoriteColor(controller, bot, message, userId) {
 
 }
 
-function query(convo) {
-    var http = require("https");
-
-    var options = {
-        "method": "GET",
-        "hostname": [
-            "cdetsng",
-            "cisco",
-            "com"
-        ],
-        "path": [
-            "wsapi",
-            "LTS-5.0",
-            "api",
-            "bug",
-            "CSCun66310",
-            "note",
-            "Release-note"
-        ],
-        "headers": {
-            "Authorization": "OAuth oauth_consumer_key=\\\"8f21b566-3ba1-4aa8-a629-e109f6c1bbb2\\\",oauth_signature_method=\\\"HMAC-SHA1\\\",oauth_timestamp=\\\"1521751302\\\",oauth_nonce=\\\"ZI9Vas1cdcS\\\",oauth_version=\\\"1.0\\\",oauth_signature=\\\"Av8E1%2B3RBB7E2j6wVtOGtWUFWEA%3D\\\"",
-            "Cache-Control": "no-cache",
-            "Postman-Token": "d2a3175d-b59a-4dd7-ac0d-67666e8c43d0"
-        }
-    };
-
-    var req = http.request(options, function (res) {
-        var chunks = [];
-
-        res.on("data", function (chunk) {
-            chunks.push(chunk);
-        });
-
-        res.on("end", function () {
-            var body = Buffer.concat(chunks);
-            convo.say('done query')
-            //console.log(body.toString());
-        });
-    });
-
-    req.end(); 
-}
